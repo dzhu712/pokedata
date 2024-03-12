692794f4ec03939d26a22fd43b5637cefd7f9409
Daniel Zhu <139385352+dzhu712@users.noreply.github.com>
2024-03-12 17:34:29 -0500

    Add Validations

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -2,4 +2,7 @@ class Pokemon < ApplicationRecord
   has_and_belongs_to_many :types
   has_and_belongs_to_many :abilities
   belongs_to :shape
+
+  validates :name, presence: true, uniqueness: true
+  validates :weight, :height, :image, presence: true
 end
--- app/models/shape.rb
+++ app/models/shape.rb
@@ -1,3 +1,5 @@
 class Shape < ApplicationRecord
   has_many :pokemons
+
+  validates :name, presence: true, uniqueness: true
 end
