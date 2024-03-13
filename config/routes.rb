804c0b985783504ea068b056e0262f39d4643942
Daniel Zhu <139385352+dzhu712@users.noreply.github.com>
2024-03-12 22:30:50 -0500

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  root 'pokemons#index'
+  resources :pokemons, only: [:index, :show]
+  resources :shapes, only: [:index, :show]
+  resources :types, only: [:index, :show]
+  resources :abilities, only: [:index, :show]
+  resources :about, only: [:index]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
