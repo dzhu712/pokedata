c886339696802ca8e5a25748cfb4f97f9dd5cc68
Daniel Zhu <139385352+dzhu712@users.noreply.github.com>
2024-03-14 15:53:33 -0500

    Add Search Feature

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,6 +1,16 @@
 class PokemonsController < ApplicationController
   def index
-    @pokemons = Pokemon.page(params[:page]).per(15)
+    @pokemons = Pokemon.all
+
+    if params[:search].present?
+      @pokemons = @pokemons.where("pokemons.name LIKE ?", "%#{params[:search]}%")
+    end
+
+    if params[:type].present?
+      @pokemons = @pokemons.joins(:types).where(types: { name: params[:type] })
+    end
+
+    @pokemons = @pokemons.page(params[:page]).per(15)
   end
 
   def show
